2caad44d4d3d3c50117282a2811a8658e02b85bc
HugoMoura88 <1DAM@A11-PC16>
2025-11-18 12:04:26 +0100

    Completo

--- Reto2HugoInigo/src/Reto/Reto2.java
+++ Reto2HugoInigo/src/Reto/Reto2.java
@@ -20,7 +20,10 @@ public class Reto2 {
 		System.out.println(menu);
 		opcion = Funciones.dimeEntero("Opcion: ", sc);
 			switch (opcion) {
-			case 1 -> Funciones2.menuTemperatura(sc);
+			case 1 -> Funciones1.menuLongitud(sc);
+			case 2 -> Funciones1.menuPeso(sc);
+			case 3 -> Funciones2.menuTemperatura(sc);
+			case 4 -> Funciones2.menuVolumen(sc);
 			default -> System.out.println("Opcion no valida"); 
 			}
 		} while (opcion!=0);
