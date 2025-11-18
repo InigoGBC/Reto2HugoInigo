4138fb227bb176f4ffeb8508667dd234aa327789
HugoMoura88 <1DAM@A11-PC16>
2025-11-18 11:45:03 +0100

    Segundo

--- Reto2HugoInigo/src/Reto/Funciones2.java
+++ Reto2HugoInigo/src/Reto/Funciones2.java
@@ -1,5 +1,21 @@
 package Reto;
 
 public class Funciones2 {
-
+	
+	public static double celsiusFarenheit(double celsius) {
+		return (celsius*9/5)+32;
+	}
+	public static double farenheitCelsius(double farenheit) {
+		return (farenheit-32)*5/9;
+	}
+	public static double litrosgalones(double litros) {
+		return litros/3.785;
+	}
+	public static double galoneslitros(double galones) {
+		return galones*3.785;
+	}
+	
+	
+	
+	
 }
--- Reto2HugoInigo/src/Reto/Reto2.java
+++ Reto2HugoInigo/src/Reto/Reto2.java
@@ -1,9 +1,38 @@
 package Reto;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Reto2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+		int opcion = 1;
+		do {
+		String menu = """ 
+				Elige la operacion:
+				1. Longitud
+				2. Peso
+				3. Temperatura
+				4. Volumen
+				0. Salir
+				""";
+		System.out.println(menu);
+		opcion = Funciones.dimeEntero("Opcion: ", sc);
+			switch (opcion) {
+			case 1 -> System.out.println("Resultado: "+Funciones1.sumar(sc));
+			case 2 -> System.out.println("Resultado: "+Funciones1.restar(sc));
+			case 3 -> System.out.println("Resultado: "+Funciones2.multiplicar(sc));
+			case 4 -> System.out.println("Resultado: "+Funciones2.dividir(sc));
+			default -> System.out.println("Opcion no valida"); 
+			}
+		} while (opcion!=0);
+				
+				
+		
+		
+		
+		
 
 	}
 
